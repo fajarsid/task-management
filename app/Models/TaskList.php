6b9d89c8c686142451e68bbf27e61637343aec6e
fajarsid <fajarsuganda79@gmail.com>
2025-08-26 23:13:48 +0700

    first commit

---
+++ app/Models/TaskList.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TaskList extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id',
+    ];
+
+    public function tasks() : HasMany
+    {
+        return $this->hasMany(Task::class, 'list_id');
+    }
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
+
+
